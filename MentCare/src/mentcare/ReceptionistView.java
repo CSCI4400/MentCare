5da3b32def2b126b51a96a947eb70bc45ab5eb51
NYeatman <ndyeatman@gmail.com>
2017-03-04 17:10:43 -0500

    Uploading latest code with finished Receptionist View

--- MentCare/src/mentcare/ReceptionistView.java
+++ MentCare/src/mentcare/ReceptionistView.java
@@ -40,6 +40,8 @@ public class ReceptionistView extends Application {
 	
 	static Stage window;
 	static Button logoutButton = new Button("Log Out");
+	static String patientsearch = "Search";
+	static String pid;
 	
 	static Stage exitwindow;
 	static Scene mainmenu;
@@ -64,8 +66,8 @@ public class ReceptionistView extends Application {
 	static Button addpatientbutton = new Button("Click to add a new patient");
 	static Button logoutbutton = new Button("Log out");
 	static Button diagnosishistorybutton = new Button("Diagnosis History");
-	static Button editrecordbutton = new Button("Edit Record");
-	//static Button searchbutton = new Button(patientsearch);
+	static Button editrecordbutton = new Button("Update Personal Information");
+	static Button searchbutton = new Button(patientsearch);
 	static Button yesbutton = new Button("yes");
 	static Button nobutton = new Button("no");
 	static Button backbutton = new Button("Back");
@@ -96,6 +98,14 @@ public class ReceptionistView extends Application {
 			}
 		});
 		
+		//Configure button actions
+		addpatientbutton.setOnAction(e -> addPatient());
+		patientrecordsbutton.setOnAction(e-> patientsearch());
+		
+		createappointmentbutton.setOnAction(e -> {
+			//Put code here for going to appointment view
+		});
+		
 		
 		GridPane grid = new GridPane();
 		grid.setHgap(10);
@@ -108,8 +118,8 @@ public class ReceptionistView extends Application {
 		grid.add(viewappointments, 3, 2);
 		
 		
-		Scene scene1 = new Scene(grid, 500, 500);
-		window.setScene(scene1);
+		mainmenu = new Scene(grid, 500, 500);
+		window.setScene(mainmenu);
 		window.show();
 	}
 	
@@ -144,6 +154,140 @@ public class ReceptionistView extends Application {
 	private static void exitProgram() {	
 		window.close();
 	}
+	
+	private static void addPatient() {
+		BorderPane addpatientlayout = new BorderPane();
+		okbutton.setOnAction(e -> {
+			window.setScene(mainmenu); //return to mainmenu
+		});
+		
+		cancelbutton.setOnAction(e -> window.setScene(mainmenu));
+		HBox layout2 = new HBox(20);
+		layout2.getChildren().addAll(okbutton, cancelbutton);
+		addpatientlayout.setBottom(layout2);
+		addpatient = new Scene(addpatientlayout, 640, 480);
+		window.setScene(addpatient);
+	}
+	
+	private static void patientsearch() {
+		
+		String selectPStmt = "SELECT * FROM mentcare.Personal_Info WHERE ? = mentcare.Personal_Info.PNumber";
+		String selectMStmt = "SELECT Last_Visit FROM mentcare.Medical_Info WHERE ? = mentcare.Medical_Info.PNum";
+		
+		VBox layout2 = new VBox(20);
+		TextField patientidinput = new TextField();
+		backbutton.setOnAction(e -> window.setScene(mainmenu));
+		layout2.getChildren().addAll(patientidl, patientidinput, searchbutton, backbutton);
+		searchbutton.setOnAction(e -> {
+			pid = patientidinput.getText();
+			int pnum = -1; //The variables passed to the 'patientrcords' method are initiated to blank values
+			String fname = "", lname = "", address = "", sex = "", phonenum = "", diagnosis="", ssn="";
+			Date bdate = null, lastvisit = null;
+			try {
+				PreparedStatement pstmt = viewMenu.con.prepareStatement(selectPStmt);
+				pstmt.setInt(1, Integer.parseInt(pid));
+				ResultSet rs = pstmt.executeQuery();
+				while(rs.next()){
+					pnum = rs.getInt("PNumber");
+					fname = rs.getString("Fname");
+					lname = rs.getString("Lname");
+					address = rs.getString("Address");
+					sex = rs.getString("Sex");
+					phonenum = rs.getString("Phone_Number");
+					bdate = rs.getDate("BDate");
+				}
+				
+				pstmt = viewMenu.con.prepareStatement(selectMStmt);
+				pstmt.setInt(1,Integer.parseInt(pid));
+				rs = pstmt.executeQuery();
+				while(rs.next()){
+					lastvisit = rs.getDate("Last_Visit");
+				}
+				pstmt.close();
+				rs.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			
+			
+			patientrecords(Integer.toString((pnum)), fname, lname, bdate.toString(), address, sex, phonenum, lastvisit.toString());
+		});
+		window.setTitle(patientsearch);
+		Scene patientsearch= new Scene(layout2, 640, 640);
+		
+		window.setScene(patientsearch);
+	}
+	
+	private static void patientrecords(String patientid, String firstnamestr, String lastnamestr, String birthdatestr, String homeaddressstr, String genderstr, String phonenumberstr, String lastvisitstring) {
+		VBox layout2 = new VBox(10);
+		Label firstname = new Label(firstnamestr); Label lastname = new Label(lastnamestr); Label birthdate = new Label(birthdatestr);
+		Label homeaddress = new Label(homeaddressstr); Label gender = new Label(genderstr); Label phonenumber = new Label(phonenumberstr);
+		Label lastvisit = new Label(lastvisitstring);
+		backbutton.setOnAction(e->patientsearch());
+		editrecordbutton.setOnAction(e-> recordedit(patientid, firstnamestr, lastnamestr, birthdatestr, homeaddressstr, genderstr, phonenumberstr, lastvisitstring));
+		layout2.getChildren().addAll(firstnamel, firstname, lastnamel, lastname, birthdatel, birthdate, homeaddressl, homeaddress, genderl, gender, phonenumberl, phonenumber, lastvisitl, lastvisit, editrecordbutton, backbutton);
+		Scene patientrecords = new Scene(layout2, 640, 640);
+		window.setScene(patientrecords);
+	}
+
+	private static void recordedit(String patientid, String firstnamestr, String lastnamestr, String birthdatestr,
+			String homeaddressstr, String genderstr, String phonenumberstr, String lastvisitstring) {
+		
+		String updatePersonalInfo = "UPDATE mentcare.Personal_Info SET Fname = ? , Lname = ?, BDate = ?, Address = ?, Sex = ?, Phone_Number = ? WHERE PNumber = ? ";
+		String updateMedicalInfo = "UPDATE mentcare.Medical_Info SET Last_Visit = ? WHERE PNum = ?";
+		
+		VBox layout3 = new VBox(10);
+		Date BirthDate;
+		TextField fname = new TextField(firstnamestr); TextField lname = new TextField(lastnamestr); TextField birthdate = new TextField(birthdatestr);
+		TextField addr = new TextField(homeaddressstr); TextField sex = new TextField(genderstr); TextField phonenum = new TextField(phonenumberstr);
+		TextField lastapt = new TextField(lastvisitstring);
+		
+		updatebutton.setOnAction(e-> {
+			try{
+			Connection Con;
+			PreparedStatement pstmt;
+			
+			Con = DriverManager.getConnection("jdbc:mysql://164.132.49.5:3306", "mentcare", "mentcare1");
+			pstmt = Con.prepareStatement(updatePersonalInfo);//Updates the personal info and possibly last visit date in medical info
+			pstmt.setString(1, fname.getText());
+			pstmt.setString(2,  lname.getText());
+			pstmt.setObject(3, LocalDate.parse(birthdate.getText()));
+			pstmt.setString(4, addr.getText());
+			pstmt.setString(5,  sex.getText());
+			pstmt.setString(6, phonenum.getText());
+			pstmt.setInt(7, Integer.parseInt(patientid));
+			pstmt.executeUpdate();
+			pstmt = Con.prepareStatement(updateMedicalInfo);
+			pstmt.setObject(1, LocalDate.parse(lastapt.getText()));
+			pstmt.setInt(2, Integer.parseInt(pid));
+			pstmt.executeUpdate();
+			pstmt.close();
+			
+			patientrecords(patientid, fname.getText(), lname.getText(), birthdate.getText(), addr.getText(), sex.getText(), phonenum.getText(), lastapt.getText());
+			
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		});
+		
+		
+		
+		
+		backbutton.setOnAction(e-> patientrecords(patientid, fname.getText(), lname.getText(), birthdate.getText(), addr.getText(), sex.getText(), phonenum.getText(), lastapt.getText()));
+		
+		layout3.getChildren().addAll(firstnamel, fname, lastnamel, lname, birthdatel, birthdate, homeaddressl, addr, genderl, sex, phonenumberl, phonenum, lastvisitl, lastapt, updatebutton, backbutton);
+		
+		
+		Scene recordeditor = new Scene(layout3, 680, 680);
+		window.setScene(recordeditor);
+		
+		
+		
+		
+	}
+	
 	
 	
 }
