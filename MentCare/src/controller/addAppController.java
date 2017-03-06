031c6fc531139cbf3018156d13ca131af601143a
Anna Blasingame <amblas5687@ung.edu>
2017-03-06 18:02:24 -0500

    Finished add controller
    prevents user from submitting unless time is selected

--- MentCare/src/controller/addAppController.java
+++ MentCare/src/controller/addAppController.java
@@ -1,3 +1,11 @@
+//TODO
+/*
+ * need to add error checking for fields for patient name, patient number, and emergency contact 
+ * 
+ * the program won't enable the submit button until a time is selected, it also disables all time values, only way to
+ * enable the time values is by selecting a date, the program essentially "self polices" that part
+ */
+
 package controller;
 
 import java.sql.Connection;
@@ -28,6 +36,7 @@ public class addAppController {
         Scene scene;
         Parent root;
 
+        //buttons for times
         @FXML private RadioButton rb1;
         @FXML private RadioButton rb2;
         @FXML private RadioButton rb3;
@@ -39,20 +48,19 @@ public class addAppController {
         @FXML private RadioButton rb11;
         @FXML private RadioButton rb12;
 
-         @FXML
-        private TextField patientName;
-        @FXML
-        private DatePicker datePick;
-        @FXML
-        private Button submit;
-        @FXML
-        private Button cancel;
-          @FXML
-          private TextField PnumTF;
+         @FXML private TextField patientName;
+        @FXML private DatePicker datePick;
+        @FXML private Button submit;
+        @FXML private Button cancel;
+        @FXML private TextField PnumTF;
 
+        //make group for radio buttons
          @FXML private ToggleGroup rg = new ToggleGroup();
-
-         //groups radio buttons into toggle
+         
+         //variable to hold the current selected time 
+         private String selectedTime; 
+         
+         //groups radio buttons into toggle, disable buttons, set values
          public void initialize()
         {
                 //set values
@@ -72,22 +80,37 @@ public class addAppController {
                 //--------------------
 
                  //add to toggle groups
+                //morning
                 rb8.setToggleGroup(rg);
                 rb9.setToggleGroup(rg);
                 rb10.setToggleGroup(rg);
                 rb11.setToggleGroup(rg);
                 rb12.setToggleGroup(rg);
 
-                 rb1.setToggleGroup(rg);
+                //afternoon
+                rb1.setToggleGroup(rg);
                 rb2.setToggleGroup(rg);
                 rb3.setToggleGroup(rg);
                 rb4.setToggleGroup(rg);
                 rb5.setToggleGroup(rg);
 
-                 //disable the submit button until all dates are checked to prevent dumb users from fucking shit up
+                 //disable the submit button and all select buttons 
+                //until all dates are checked and a time is selected to prevent dumb users from fucking shit up
                 submit.setDisable(true);
+                
+                rb8.setDisable(true);
+                rb9.setDisable(true);
+                rb10.setDisable(true);
+                rb11.setDisable(true);
+                rb12.setDisable(true);
+                rb1.setDisable(true);
+                rb2.setDisable(true);
+                rb3.setDisable(true);
+                rb4.setDisable(true);
+                rb5.setDisable(true);    
          }
 
+         
         //always reference main method, and build constructor
         private MainFXApp main;
         public void setMain(MainFXApp mainIn)
@@ -96,7 +119,223 @@ public class addAppController {
         }
 
 
-    //button to go back, not really using
+      //called when a radio button is selected, assigns value to radio button, only enables submit when radio button is selected
+        @FXML
+        void timeSelect(ActionEvent event) 
+        {
+
+        	//set the time value 
+        	selectedTime = rg.getSelectedToggle().getUserData().toString();
+        	//System.out.println("selectedTime " + selectedTime);
+        	
+        	//enable the submit button if they actually select a time 
+        	submit.setDisable(false);
+        }
+
+
+        //this is where you want to connect to the database and check appointment times, disables all the filled times
+        @FXML
+        void checkDate (ActionEvent event) throws SQLException
+        {
+        	//enable all time buttons when different date selected
+    		rb8.setDisable(false);
+    		rb9.setDisable(false);
+    		rb10.setDisable(false);
+    		rb11.setDisable(false);
+    		rb12.setDisable(false);
+    		rb1.setDisable(false);
+    		rb2.setDisable(false);
+    		rb3.setDisable(false);
+    		rb4.setDisable(false);
+    		rb5.setDisable(false);
+    		
+    		//deselect time buttons
+    		rb8.setSelected(false);
+    		rb9.setSelected(false);
+    		rb10.setSelected(false);
+    		rb11.setSelected(false);
+    		rb12.setSelected(false);
+    		rb1.setSelected(false);
+    		rb2.setSelected(false);
+    		rb3.setSelected(false);
+    		rb4.setSelected(false);
+    		rb5.setSelected(false);
+    		
+    		//disable the submit button in case users are moving around
+    		submit.setDisable(true);
+
+    		
+            //make a time variable to catch current times from database
+            String curTime;
+
+            //create query to send appt in db
+            String dateChecking = "SELECT apTime FROM `Current_Appointment` WHERE apDate = ?";
+            ResultSet rs = null;
+
+            //connect to database
+            try(
+                            Connection conn = DBConfig.getConnection();
+                            PreparedStatement checkDates = conn.prepareStatement(dateChecking);
+                    ){
+
+                            //insert the selected date for the query
+                            checkDates.setString(1, datePick.getValue().toString());
+                            rs = checkDates.executeQuery();//execute query
+
+                            // move through and grab info
+                            while (rs.next())
+                            {
+                                   //grab time to check
+                                   curTime = (String) rs.getObject(1);
+
+                                 //start comparing times and disabling the buttons
+                   				if(curTime.compareTo("8")==0)
+                   				{
+                   					rb8.setDisable(true);
+                   					rb8.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("9")==0)
+                   				{
+                   					rb9.setDisable(true);
+                   					rb9.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("10")==0)
+                   				{
+                   					rb10.setDisable(true);
+                   					rb10.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("11")==0)
+                   				{
+                   					rb11.setDisable(true);
+                   					rb11.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("12")==0)
+                   				{
+                   					rb12.setDisable(true);
+                   					rb12.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("1")==0)
+                   				{
+                   					rb1.setDisable(true);
+                   					rb1.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("2")==0)
+                   				{
+                   					rb2.setDisable(true);
+                   					rb2.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("3")==0)
+                   				{
+                   					rb3.setDisable(true);
+                   					rb3.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("4")==0)
+                   				{
+                   					rb4.setDisable(true);
+                   					rb4.setSelected(false);
+                   				}
+                   				if(curTime.compareTo("5")==0)
+                   				{
+                   					rb5.setDisable(true);
+                   					rb5.setSelected(false);
+                   				}
+                            }
+            }catch(SQLException ex){//try
+                           ex.printStackTrace();
+                    }finally{//catch
+                           if(rs != null)
+                           {
+                                   rs.close();
+                           }
+                    }
+            //System.out.println("Date selected" + datePick.getValue().toString());
+        }//end method
+        
+        
+    //this is the submit button where appointments are submitted
+    @FXML
+    void submitAppt(ActionEvent event) {
+
+        //grab text fields and dates, time is handled in seperate method and passed into global variable
+        String date = datePick.getValue().toString();
+        String name = patientName.getText();//error checking needed
+        String patientNum = PnumTF.getText();//error checking needed
+        
+        
+        //printing purpose 
+        System.out.println("Date selected: " + date);
+        System.out.println("Time selected: " + rg.getSelectedToggle().getUserData().toString());
+
+
+        //create query to send appt in db
+        String apptQuery = "INSERT INTO `Current_Appointment`(`Pnum`, `Pname`, `DocID`, `apDate`, `apTime`) "
+                        + "VALUES (?,?,?,?,?)";
+
+
+        //try to connect to db
+        try (Connection conn = DBConfig.getConnection();
+                               PreparedStatement createAppt = conn.prepareStatement(apptQuery,Statement.RETURN_GENERATED_KEYS);)
+                {
+
+        //set info into the query
+        createAppt.setString(1, patientNum);
+        createAppt.setString(2, name);
+        createAppt.setString(3, "0");
+        createAppt.setString(4, date);
+        createAppt.setString(5, selectedTime);
+
+        //print query
+        System.out.println("Query Sent" + createAppt.toString());
+
+        //pass the query in
+        int affectedRow  = createAppt.executeUpdate();
+
+        //clear text fields
+        if(affectedRow == 1)
+        {
+                patientName.clear();
+                PnumTF.clear();
+                
+                //can't clear date values yet, causes a null pointer error
+               //datePick.getEditor().clear();
+                //datePick.setValue(null);
+                
+                //now that you've submitted, disable everything
+                submit.setDisable(true);
+                
+                rb8.setDisable(true);
+                rb9.setDisable(true);
+                rb10.setDisable(true);
+                rb11.setDisable(true);
+                rb12.setDisable(true);
+                rb1.setDisable(true);
+                rb2.setDisable(true);
+                rb3.setDisable(true);
+                rb4.setDisable(true);
+                rb5.setDisable(true);
+                
+              //deselect time buttons
+        		rb8.setSelected(false);
+        		rb9.setSelected(false);
+        		rb10.setSelected(false);
+        		rb11.setSelected(false);
+        		rb12.setSelected(false);
+        		rb1.setSelected(false);
+        		rb2.setSelected(false);
+        		rb3.setSelected(false);
+        		rb4.setSelected(false);
+        		rb5.setSelected(false);
+        }
+
+
+                }catch(SQLException ex)//try
+                {
+                       DBConfig.displayException(ex);
+                }
+    }//end method
+    
+    
+  //go back to main
     @FXML
         void ClickCancelButton(ActionEvent event) throws Exception {
        try{
@@ -112,174 +351,5 @@ public class addAppController {
                 e.printStackTrace();
                 }
     }
-
-
-    //this is the submit button where appointments are submitted
-    @FXML
-    void submitAppt(ActionEvent event) {
-
-        //check for error--possibly check name and patient number
-        String date = datePick.getValue().toString();
-        String time = radioBttnSelect(event);
-        String name = patientName.getText();
-        String patientNum = PnumTF.getText();
-
-        System.out.println("Date: " + date);
-        System.out.println("Time: " + time);
-
-
-        //create query to send appt in db
-        String apptQuery = "INSERT INTO `Current_Appointment`(`Pnum`, `Pname`, `DocID`, `apDate`, `apTime`) "
-                        + "VALUES (?,?,?,?,?)";
-
-
-        //try to connect to db
-        try (Connection conn = DBConfig.getConnection();
-                               PreparedStatement createAppt = conn.prepareStatement(apptQuery,Statement.RETURN_GENERATED_KEYS);)
-                {
-        //System.out.println("Time" + time);
-
-        //set info into the query
-        createAppt.setString(1, patientNum);
-        createAppt.setString(2, name);
-        createAppt.setString(3, "0");
-        createAppt.setString(4, date);
-        createAppt.setString(5, time);
-
-        //System.out.println("Query Sent" + apptQuery);
-
-
-        //pass the query in
-        int affectedRow  = createAppt.executeUpdate();
-
-        //clear text fields
-        /*if(affectedRow == 1)
-        {
-                //clear the text fields
-        }*/
-
-
-                }catch(SQLException ex)//try
-                {
-                       DBConfig.displayException(ex);
-                }
-    }
-
-
-    //get the value from the radio button
-    private String radioBttnSelect(ActionEvent action)
-    {
-        return rg.getSelectedToggle().getUserData().toString();
-    }
-
-
-    //this is where you want to connect to the database and check appointment times
-    @FXML
-    void checkDate (ActionEvent event) throws SQLException
-    {
-        //disable button every time they change dates because users are fucking idiots
-        submit.setDisable(true);
-
-        //enable all time buttons when different date selected
-                       rb8.setDisable(false);
-                       rb9.setDisable(false);
-                       rb10.setDisable(false);
-                       rb11.setDisable(false);
-                       rb12.setDisable(false);
-                       rb1.setDisable(false);
-                       rb2.setDisable(false);
-                       rb3.setDisable(false);
-                       rb4.setDisable(false);
-                       rb5.setDisable(false);
-
-        //make a time variable to catch current times from database
-        String curTime;
-
-        //create query to send appt in db
-        String dateChecking = "SELECT apTime FROM `Current_Appointment` WHERE apDate = ?";
-        ResultSet rs = null;
-
-        //connect to database
-        try(
-                        Connection conn = DBConfig.getConnection();
-                        PreparedStatement checkDates = conn.prepareStatement(dateChecking);
-                ){
-
-                        //insert the selected date for the query
-                        checkDates.setString(1, datePick.getValue().toString());
-                        rs = checkDates.executeQuery();//execute query
-
-                        // move through and grab info
-                        while (rs.next())
-                        {
-                               //grab time to check
-                               curTime = (String) rs.getObject(1);
-
-                               //start comparing times and disabling the buttons
-                               if(curTime.compareTo("8")==0)
-                               {
-                                       rb8.setDisable(true);
-                                       rb8.setSelected(false);
-                               }
-                               if(curTime.compareTo("9")==0)
-                               {
-                                       rb9.setDisable(true);
-                                       rb9.setSelected(false);
-                               }
-                               if(curTime.compareTo("10")==0)
-                               {
-                                       rb10.setDisable(true);
-                                       rb10.setSelected(false);
-                               }
-                               if(curTime.compareTo("11")==0)
-                               {
-                                       rb11.setDisable(true);
-                                       rb11.setSelected(false);
-                               }
-                               if(curTime.compareTo("12")==0)
-                               {
-                                       rb12.setDisable(true);
-                                       rb12.setSelected(false);
-                               }
-                               if(curTime.compareTo("1")==0)
-                               {
-                                       rb1.setDisable(true);
-                                       rb1.setSelected(false);
-                               }
-                               if(curTime.compareTo("2")==0)
-                               {
-                                       rb2.setDisable(true);
-                                       rb2.setSelected(false);
-                               }
-                               if(curTime.compareTo("3")==0)
-                               {
-                                       rb3.setDisable(true);
-                                       rb3.setSelected(false);
-                               }
-                               if(curTime.compareTo("4")==0)
-                               {
-                                       rb4.setDisable(true);
-                                       rb4.setSelected(false);
-                               }
-                               if(curTime.compareTo("5")==0)
-                               {
-                                       rb5.setDisable(true);
-                                       rb5.setSelected(false);
-                               }
-                               //enable the submit after checking times
-                               submit.setDisable(false);
-                        }
-        }catch(SQLException ex){//try
-                       ex.printStackTrace();
-                }finally{//catch
-                       submit.setDisable(false);//if it made it through and there were no times at all, enables submit
-                       if(rs != null)
-                       {
-                               rs.close();
-                       }
-                }
-        System.out.println("Date selected" + datePick.getValue().toString());
-    }//end method
-
 
 }//end class
