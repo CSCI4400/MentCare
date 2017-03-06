6bef2ccfbe1238d2481dcee3da016a21f613487a
kelleyvr <kelley@linuxsa.net>
2017-03-06 17:39:55 -0500

    Update InternetConnection.java

--- src/application/InternetConnection.java
+++ src/application/InternetConnection.java
@@ -9,24 +9,27 @@ import java.net.UnknownHostException;
 public class InternetConnection {
 
     //May want to use InetAddress.isReachable if this doesn't meet our needs.
-    public static void checkInternet() {
+    public static boolean checkInternet() {
         try {
             URL url = new URL("https://www.google.com");
             URLConnection connection = url.openConnection();
             connection.connect();
             System.out.println("Internet connection was successful");
+            return true;
         } catch (UnknownHostException e) {
             System.out.println("Internet connection was not successful");
             e.printStackTrace();
         } catch (MalformedURLException e) {
             System.out.println("Internet connection was not successful");
             e.printStackTrace();
+            return false;
         } catch (IOException e) {
             System.out.println("Internet connection was not successful");
             e.printStackTrace();
+            return false;
         }
     }
         public static void main(String[] args) {
-        checkInternet();
+            checkInternet();            
     }
 }
