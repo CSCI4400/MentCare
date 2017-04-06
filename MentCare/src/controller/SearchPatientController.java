4608ae1d48d898a1ba073568d79f2b31dc8c020a
NYeatman <ndyeatman@gmail.com>
2017-04-06 15:57:13 -0400

    Back button on search patient now returns to correct main menu

--- MentCare/src/controller/SearchPatientController.java
+++ MentCare/src/controller/SearchPatientController.java
@@ -1,29 +1,34 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Patient;
 
 public class SearchPatientController {
-	
+
 	static Button backbutton = new Button("Back");
 	static Button searchbutton = new Button("Search");
 	static Label patientidl = new Label("What is the Patient ID Number?");
 	static Patient a = new Patient();
 	static String pid; //used to store the ID# of the patient whose record is being looked at
 	static String patientsearch = "Search";
-	
+
 	public static void searchPatientDoc(Stage window){
 		VBox layout2 = new VBox(20);
 		TextField patientidinput = new TextField();
 		DoctorViewController docView = new DoctorViewController();
 		backbutton.setOnAction(e-> {
 			try {
-				docView.start(window);
+				AnchorPane mainv = (AnchorPane) FXMLLoader.load(mainViewController.class.getResource("/view/mainView.fxml"));
+				Scene scene = new Scene(mainv,600,400);
+				window.setScene(scene);
 			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -43,9 +48,9 @@ public class SearchPatientController {
 		Scene patientsearchDoc = new Scene(layout2, 640, 640);
 
 		window.setScene(patientsearchDoc);
-		
+
 	}
-	
+
 	public static void searchPatientRecep(Stage window){
 		VBox layout2 = new VBox(20);
 		TextField patientidinput = new TextField();
@@ -70,9 +75,9 @@ public class SearchPatientController {
 		});
 		window.setTitle(patientsearch);
 		Scene patientsearchRecep = new Scene(layout2, 640, 640);
-		
+
 		window.setScene(patientsearchRecep);
-		
+
 	}
 
 }
